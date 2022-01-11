d9d4ec7145c510669908f254766d1bab6aec8ad6
Ssemi <ssemibiz@yahoo.co.kr>
2022-01-11 20:37:52 +0900

    init Worker_A

---
+++ worker-A/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using common_module;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using worker_A;
+
+Console.WriteLine("Hello, World!");
+
+var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var env = hostContext.HostingEnvironment;
+                    var config = hostContext.Configuration;
+
+                    services.AddTransient<IWorker, Worker>();
+                    services.AddTransient<Runner>();
+
+                })
+                .Build();
+
+var svc = ActivatorUtilities.CreateInstance<Runner>(host.Services);
+_ = svc.DoAction();
---
+++ worker-A/Worker.cs
@@ -0,0 +1,14 @@
+﻿using common_module;
+
+namespace Worker_A;
+
+public class Worker : IWorker
+{
+    public Task RunAsync(string message)
+    {
+        Console.WriteLine(message + " Worker-A");
+        
+        return Task.CompletedTask;
+    }
+}
+
